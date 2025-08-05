4bee9721b1531aa424bf57844f75fe62fef41224
VesperVanity <OCEAN.jung@protonmail.com>
2025-08-06 01:18:54 +0200

    Exercise 2.29

---
+++ chapter_2/exercise_2.29.c
@@ -0,0 +1,27 @@
+/*
+C: How to Program by Paul Deitel & Harvey Deitel
+Chapter 2 Exercise 2.29 Integer value of a character
+File created: 2025-08-06
+File last modified: 2025-08-06
+Author: VesperVanity (VV) https://github.com/VesperVanity
+*/
+#include <stdio.h>
+
+int main(void)
+{
+	printf("%d\n", 'A');
+	printf("%d\n", 'B');
+	printf("%d\n", 'C');
+	printf("%d\n", 'a');
+	printf("%d\n", 'b');
+	printf("%d\n", 'c');
+	printf("%d\n", '0');
+	printf("%d\n", '1');
+	printf("%d\n", '2');
+	printf("%d\n", '$');
+	printf("%d\n", '*');
+	printf("%d\n", '+');
+	printf("%d\n", '/');
+	printf("%d\n", ' ');
+	return 0;
+}
