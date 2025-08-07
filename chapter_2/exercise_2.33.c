5a59b2c3e0c7569e0581b101db4473d937426fdf
VesperVanity <OCEAN.jung@protonmail.com>
2025-08-07 23:45:52 +0200

    Init exercise 2.33

---
+++ chapter_2/exercise_2.33.c
@@ -0,0 +1,14 @@
+/*
+C: How to Program by Paul Deitel & Harvey Deitel
+Chapter 2 Exercise 2.32 Body Mass Index Calculator
+File created: 2025-08-07
+File last modified: 2025-08-07
+Author: VesperVanity (VV) https://github.com/VesperVanity
+*/
+#include <stdio.h>
+
+int main(void)
+{
+	
+	return 0;
+}
