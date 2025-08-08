4b8c90a5f2aba6c23a54e61c222c2c1051e89045
VesperVanity <OCEAN.jung@protonmail.com>
2025-08-08 23:55:37 +0200

    Init

--- chapter_2/exercise_2.33.c
+++ chapter_2/exercise_2.33.c
@@ -1,6 +1,6 @@
 /*
 C: How to Program by Paul Deitel & Harvey Deitel
-Chapter 2 Exercise 2.33 Car pool savings calculator
+Chapter 2 Exercise 2.33 Car Pool Savings Calculator
 File created: 2025-08-07
 File last modified: 2025-08-07
 Author: VesperVanity (VV) https://github.com/VesperVanity
