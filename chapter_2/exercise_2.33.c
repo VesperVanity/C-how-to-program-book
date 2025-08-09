895a8bffba73663b9dd48db42c10a59aaada001d
VesperVanity <OCEAN.jung@protonmail.com>
2025-08-09 23:18:35 +0200

    Hiatus C how to program book

--- chapter_2/exercise_2.33.c
+++ chapter_2/exercise_2.33.c
@@ -1,14 +1,15 @@
 /*
 C: How to Program by Paul Deitel & Harvey Deitel
-Chapter 2 Exercise 2.33 Car Pool Savings Calculator
+Chapter 2 Exercise 2.33 Car pool savings calculator
 File created: 2025-08-07
-File last modified: 2025-08-07
+File last modified: 2025-08-09
 Author: VesperVanity (VV) https://github.com/VesperVanity
 */
 #include <stdio.h>
 
 int main(void)
 {
-
+	printf("%s\n", "C: How to Program is on hiatus. 
+		Continue with this exercise when, or if, you return.");
 	return 0;
 }
